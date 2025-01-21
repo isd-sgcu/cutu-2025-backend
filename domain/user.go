fb4b0f2a664e27f895179f8a9fc6c4919e1764fb
MoosaTae <sadhis.tae@gmail.com>
2025-01-21 17:21:26 +0700

    feat: add role and assign roles based on mock phone number

--- domain/user.go
+++ domain/user.go
@@ -1,14 +1,23 @@
 package domain
 
+type Role string
+
+const (
+	Student Role = "student"
+	Staff   Role = "staff"
+	Admin   Role = "admin"
+)
+
 type User struct {
-	ID            uint   `gorm:"primaryKey"`
-	Name          string
-	Email         string
-	Phone         string
-	University    string
-	SizeJersey    string
+	ID             uint `gorm:"primaryKey"`
+	Name           string
+	Email          string
+	Phone          string
+	University     string
+	SizeJersey     string
 	FoodLimitation string
 	InvitationCode *string
-	State         string
-	ImageURL      string
+	State          string
+	ImageURL       string
+	Role           Role `json:"-"` // exclude from JSON
 }
